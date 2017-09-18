28f8aad8d4b28dfb7a278df4bed2bbd9f2980c33
majid <majid@dal.ca>
2017-09-18 16:13:03 -0300

    Updated function to create a list

--- 4task2.py
+++ 4task2.py
@@ -4,13 +4,15 @@ my_list = [1,2,3,4,5]
 
 def list_mul(my_list):
     """ This muls the list elements my 5"""
+    new_list = []
     if isinstance(my_list,list):
         for index in range(len(my_list)):
-            my_list[index] = my_list[index] * 5 
+            new_list.append(my_list[index] * 5) 
             # I can write that as my_list[index] *= 5
     else:
         print ("gimme a list!!!!")
+    return new_list
 
 print(my_list)
-list_mul(my_list)
-print(my_list)
+my_new_list = list_mul(my_list)
+print(my_new_list)
